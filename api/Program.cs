7fd791bd67bf450a743c98d305146fed4fcb35e4
Janar Kaljula <janaka@taltech.ee>
2024-09-30 19:26:51 +0300

    Finished datacontext, added team model

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,18 @@
+using api.Data;
+using api.Data.Repos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services
+    .AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("TeamUpDB"))
+    .AddScoped<PlayersRepo>();
 
 var app = builder.Build();
 
@@ -15,5 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
