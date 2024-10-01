9c33e4e877cb8dea5ab1dd2ebad4f4036acd17d2
Janar Kaljula <janaka@taltech.ee>
2024-10-01 12:19:32 +0300

    Initialized Nuxt

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77f8f3500e127678eb2ea634ed336e18df88f881")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ee8ba7ad1c76e694d459261c7894121f94e746")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
