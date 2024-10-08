537113275344edfe005ce1a170d52eac5e3e830d
Janar Kaljula <janaka@taltech.ee>
2024-10-09 00:01:29 +0300

    Configured program.cs and added docker-compose file. Postgres is running thorugh database.

--- BackEnd/REST/Program.cs
+++ BackEnd/REST/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using REST.Data;
+using REST.Data.Repos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services
+    .AddDbContext<DataContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Default")))
+    .AddScoped<PlayersRepo>();
 
 var app = builder.Build();
+
+using (var scope = ((IApplicationBuilder)app).ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+using (var context = scope.ServiceProvider.GetService<DataContext>()){
+    context?.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,5 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
