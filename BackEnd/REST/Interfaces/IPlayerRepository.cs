6b4e11947071ab95296fa4ed0afb3e7712332fa1
Janar Kaljula <janaka@taltech.ee>
2024-10-16 11:03:44 +0300

    Added repository interfaces

---
+++ BackEnd/REST/Interfaces/IPlayerRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using REST.Models.Classes;
+
+namespace REST.Interfaces
+{
+    public interface IPlayerRepository
+    {
+        Task<List<Player>> GetAllAsync();
+        Task<Player?> GetByIdAsync(int id);
+        Task<Player> CreateAsync(Player playerModel);
+        Task<Player?> UpdateAsync(int id, Player playerModel);
+        Task<Player?> DeleteAsync(int id);
+    }
+}
--- BackEnd/REST/Interfaces/ITeamRepository.cs
+++ BackEnd/REST/Interfaces/ITeamRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using REST.Dtos.Team;
 using REST.Models.Classes;
 
 namespace REST.Interfaces
