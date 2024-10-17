81f2cdfdad3fdc68d4d0cfcc707c9b3aac9d4ce4
Piia Carmen Milkov <pimilk@taltech.ee>
2024-10-17 14:24:38 +0300

    väike viga parandatud

--- FrontEnd/TeamUp/components/TeamGenerator.vue
+++ FrontEnd/TeamUp/components/TeamGenerator.vue
@@ -17,7 +17,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, watch } from 'vue';
+import { ref, reactive } from 'vue';
 import { usePlayerStore } from '~/stores/playerStore';
 import { useTeamStore } from '~/stores/teamStore'; 
 import type { Player } from '~/types/player'; 
