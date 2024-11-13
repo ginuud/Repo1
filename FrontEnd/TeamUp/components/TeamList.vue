f19dfd582c5a19583c69c38d500b4b92017adb0b
Janar Kaljula <janaka@taltech.ee>
2024-11-13 23:54:21 +0200

    Added empty team list text to team list page

--- FrontEnd/TeamUp/components/TeamList.vue
+++ FrontEnd/TeamUp/components/TeamList.vue
@@ -1,12 +1,17 @@
  
 <template>
-  <div v-for="team in teams" :key="team.id" class="team-accordion-item">
-    <div class="accordion-header">
-      <strong>Team: {{ team.name }}</strong>
-      <button @click="deleteTeam(team.id)" class="delete-button">Delete</button>
-    </div>
-    <div class="accordion-content">
-      Members: {{ team.members?.map(member => member.name).join(', ') || 'No members' }}
+  <div v-if="teams.length === 0" class="text-center text-red-500">
+  	No teams have been added   	 
+	</div>
+  <div v-else>
+    <div v-for="team in teams" :key="team.id" class="team-accordion-item">
+      <div class="accordion-header">
+        <strong>Team: {{ team.name }}</strong>
+        <button @click="deleteTeam(team.id)" class="delete-button">Delete</button>
+      </div>
+      <div class="accordion-content">
+        Members: {{ team.members?.map(member => member.name).join(', ') || 'No members' }}
+      </div>
     </div>
   </div>
 </template>
