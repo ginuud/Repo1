604e8c7717f94f724a4a7381107d70321f9b05c4
Janar Kaljula <janaka@taltech.ee>
2024-11-20 17:14:46 +0200

    shadcn init

--- FrontEnd/TeamUp/nuxt.config.ts
+++ FrontEnd/TeamUp/nuxt.config.ts
@@ -2,7 +2,24 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxt/fonts'],
+  modules: [
+    '@nuxt/ui',
+    '@pinia/nuxt',
+    '@nuxt/fonts',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
   imports :{dirs: ["types/*.ts"]},
   runtimeConfig: {
     public: {exercisesApiUrl: "https://localhost:5181/api/"}
