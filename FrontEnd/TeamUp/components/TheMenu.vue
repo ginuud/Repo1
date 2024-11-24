f42dc106dd71487f861ac1de245203d7496b04cf
Janar Kaljula <janaka@taltech.ee>
2024-11-24 21:39:25 +0200

    changed add button placement logic

--- FrontEnd/TeamUp/components/TheMenu.vue
+++ FrontEnd/TeamUp/components/TheMenu.vue
@@ -19,13 +19,10 @@ const links = ref([
 
 <template>
   <div class="flex items-center justify-between px-8 py-2">
-    <!-- Logo Image -->
     <img src="@/components/teamup.png" alt="TeamUp logo" class="w-24 h-auto" />
 
-    <!-- Spacer -->
     <div class="flex-1 pl-10"></div>
 
-    <!-- Navigation Bar -->
     <UHorizontalNavigation
       v-if="route.name !== 'index'"
       :links="links"
@@ -36,7 +33,6 @@ const links = ref([
         base: 'rounded-md font-medium text-xl px-4 py-8',
       }"
     />
-    <AddPlayer/>
   </div>
 </template>
 
--- FrontEnd/TeamUp/layouts/default.vue
+++ FrontEnd/TeamUp/layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <TheMenu />
+    <div class="flex items-center justify-between px-8 py-2">
+      <TheMenu />
+      <div class="flex-1"></div>
+      <div><AddPlayer class="items-center"/></div>
+    </div>
     <div class="mt-4">
       <slot />
     </div>
